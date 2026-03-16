f8f8000eb2aa6bc18dabd74ad7f9504471db1d0b
svyasa <ajith-s@svyasa-sas.edu.in>
2026-03-16 20:14:55 +0530

    updated

--- add.c
+++ add.c
@@ -1,9 +1,7 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-printf("Welcome to S-Vyasa\n");
-printf("School of CSA\n");
-printf("Hello");
-return 0;
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 3;
+    printf("Sum = %d\n", a + b);
+    return 0;
 }
